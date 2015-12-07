3f2c1dc40f696a5f3fef96434a36b2218d56cff2
csteelatgburg <chuck@steelfarms.org>
2015-12-07 16:03:44 -0500

    Initial upload

---
+++ USMT Front End/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StoreLocation = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.StoreFolderTextBox = new System.Windows.Forms.TextBox();
+            this.SetStoreFolder = new System.Windows.Forms.Button();
+            this.StoreProfile = new System.Windows.Forms.RadioButton();
+            this.LoadProfile = new System.Windows.Forms.RadioButton();
+            this.Command = new System.Windows.Forms.Label();
+            this.Users = new System.Windows.Forms.ListBox();
+            this.Directions = new System.Windows.Forms.Label();
+            this.Execute = new System.Windows.Forms.Button();
+            this.ProfileSize = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // StoreLocation
+            // 
+            this.StoreLocation.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            this.StoreLocation.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Store Folder";
+            // 
+            // StoreFolderTextBox
+            // 
+            this.StoreFolderTextBox.Location = new System.Drawing.Point(86, 12);
+            this.StoreFolderTextBox.Name = "StoreFolderTextBox";
+            this.StoreFolderTextBox.Size = new System.Drawing.Size(309, 20);
+            this.StoreFolderTextBox.TabIndex = 1;
+            // 
+            // SetStoreFolder
+            // 
+            this.SetStoreFolder.Location = new System.Drawing.Point(418, 13);
+            this.SetStoreFolder.Name = "SetStoreFolder";
+            this.SetStoreFolder.Size = new System.Drawing.Size(78, 19);
+            this.SetStoreFolder.TabIndex = 2;
+            this.SetStoreFolder.Text = "Set";
+            this.SetStoreFolder.UseVisualStyleBackColor = true;
+            this.SetStoreFolder.Click += new System.EventHandler(this.SetStoreFolder_Click);
+            // 
+            // StoreProfile
+            // 
+            this.StoreProfile.AutoSize = true;
+            this.StoreProfile.Location = new System.Drawing.Point(12, 38);
+            this.StoreProfile.Name = "StoreProfile";
+            this.StoreProfile.Size = new System.Drawing.Size(82, 17);
+            this.StoreProfile.TabIndex = 3;
+            this.StoreProfile.TabStop = true;
+            this.StoreProfile.Text = "Store Profile";
+            this.StoreProfile.UseVisualStyleBackColor = true;
+            // 
+            // LoadProfile
+            // 
+            this.LoadProfile.AutoSize = true;
+            this.LoadProfile.Location = new System.Drawing.Point(100, 38);
+            this.LoadProfile.Name = "LoadProfile";
+            this.LoadProfile.Size = new System.Drawing.Size(81, 17);
+            this.LoadProfile.TabIndex = 4;
+            this.LoadProfile.TabStop = true;
+            this.LoadProfile.Text = "Load Profile";
+            this.LoadProfile.UseVisualStyleBackColor = true;
+            // 
+            // Command
+            // 
+            this.Command.AutoSize = true;
+            this.Command.Location = new System.Drawing.Point(16, 210);
+            this.Command.Name = "Command";
+            this.Command.Size = new System.Drawing.Size(0, 13);
+            this.Command.TabIndex = 5;
+            // 
+            // Users
+            // 
+            this.Users.FormattingEnabled = true;
+            this.Users.Location = new System.Drawing.Point(12, 61);
+            this.Users.Name = "Users";
+            this.Users.Size = new System.Drawing.Size(169, 134);
+            this.Users.TabIndex = 6;
+            this.Users.SelectedIndexChanged += new System.EventHandler(this.Users_SelectedIndexChanged);
+            // 
+            // Directions
+            // 
+            this.Directions.AutoSize = true;
+            this.Directions.Location = new System.Drawing.Point(266, 44);
+            this.Directions.Name = "Directions";
+            this.Directions.Size = new System.Drawing.Size(265, 13);
+            this.Directions.TabIndex = 7;
+            this.Directions.Text = "Step 1. Click the Set button to browse for a store folder";
+            // 
+            // Execute
+            // 
+            this.Execute.Location = new System.Drawing.Point(481, 203);
+            this.Execute.Name = "Execute";
+            this.Execute.Size = new System.Drawing.Size(83, 27);
+            this.Execute.TabIndex = 8;
+            this.Execute.Text = "Execute";
+            this.Execute.UseVisualStyleBackColor = true;
+            this.Execute.Click += new System.EventHandler(this.Execute_Click);
+            // 
+            // ProfileSize
+            // 
+            this.ProfileSize.AutoSize = true;
+            this.ProfileSize.Location = new System.Drawing.Point(199, 67);
+            this.ProfileSize.Name = "ProfileSize";
+            this.ProfileSize.Size = new System.Drawing.Size(10, 13);
+            this.ProfileSize.TabIndex = 9;
+            this.ProfileSize.Text = " ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 244);
+            this.Controls.Add(this.ProfileSize);
+            this.Controls.Add(this.Execute);
+            this.Controls.Add(this.Directions);
+            this.Controls.Add(this.Command);
+            this.Controls.Add(this.LoadProfile);
+            this.Controls.Add(this.StoreProfile);
+            this.Controls.Add(this.SetStoreFolder);
+            this.Controls.Add(this.StoreFolderTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Users);
+            this.Name = "Form1";
+            this.Text = "USMT";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog StoreLocation;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox StoreFolderTextBox;
+        private System.Windows.Forms.Button SetStoreFolder;
+        private System.Windows.Forms.RadioButton StoreProfile;
+        private System.Windows.Forms.RadioButton LoadProfile;
+        private System.Windows.Forms.Label Command;
+        private System.Windows.Forms.ListBox Users;
+        private System.Windows.Forms.Label Directions;
+        private System.Windows.Forms.Button Execute;
+        private System.Windows.Forms.Label ProfileSize;
+    }
+}
+
---
+++ USMT Front End/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        string ProfilesDirectory = "";
+        public static long DirSize(DirectoryInfo d)
+        {
+            long Size = 0;
+            // Add file sizes.
+            FileInfo[] fis = d.GetFiles();
+            foreach (FileInfo fi in fis)
+            {
+                Size += fi.Length;
+            }
+            // Add subdirectory sizes.
+            DirectoryInfo[] dis = d.GetDirectories();
+            foreach (DirectoryInfo di in dis)
+            {
+                Size += DirSize(di);
+            }
+            return (Size);
+        }
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SetStoreFolder_Click(object sender, EventArgs e)
+        {
+            StoreLocation.ShowDialog();
+            DirectoryInfo di = new DirectoryInfo(StoreLocation.SelectedPath + "\\USMT");
+
+            if (di.Exists)
+            {
+                LoadProfile.Checked = true;
+                string CommandString = @"loadstate " + StoreLocation.SelectedPath + @" /i:miguser.xml /i:migapp.xml";
+                Command.Text = CommandString;
+                Directions.Text = "You have chosen a folder which contains a profile.\r\n" +
+                    "This tool will restore the contained profile to this computer.\r\n\r\n" +
+                    "Step 2: Click Execute to run the loadstate command";
+
+
+            }
+            else
+            {
+                StoreProfile.Checked = true;
+                string UserProfile = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                if(UserProfile.Contains(@"C:\Documents and Settings")) {
+                    ProfilesDirectory = "c:\\documents and settings";
+                }
+                else if (UserProfile.Contains(@"C:\Users")) {
+                    ProfilesDirectory = "C:\\Users";
+                }
+                DirectoryInfo ProfilesDir = new DirectoryInfo(ProfilesDirectory);
+                DirectoryInfo[] Profiles = ProfilesDir.GetDirectories();
+                foreach (DirectoryInfo dir in Profiles)
+                {
+                    Users.Items.Add(dir.Name);
+                }
+                Users.Refresh();
+                Directions.Text = "You have selected a directory to store a profile. \r\n\r\n" +
+                    "Step 2: Select the user to migrate from the list on the left";
+             
+
+            }
+
+            StoreFolderTextBox.Text = StoreLocation.SelectedPath;
+          
+        }
+
+        private void Users_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            string commandstring = @Directory.GetCurrentDirectory() + "\\" + "scanstate.exe " + StoreLocation.SelectedPath + @" /ue:*\* /ui:fas\" + Users.SelectedItem + @" /i:miguser.xml /i:migapp.xml /o";
+            Command.Text =  commandstring;
+            Directions.Text = "Step 3: Click Execute to store the user's profile";
+
+        }
+
+        private void Execute_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show(Directory.GetCurrentDirectory());
+            //System.Diagnostics.Process.Start( Command.Text);
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            process.StartInfo.WorkingDirectory = Directory.GetCurrentDirectory();
+            if (StoreProfile.Checked == true)
+            {
+                process.StartInfo.FileName = "scanstate.exe";
+                process.StartInfo.Arguments = StoreLocation.SelectedPath + @" /ue:*\* /ui:fas\" + Users.SelectedItem + @" /i:miguser.xml /i:migapp.xml /o";
+                process.Start();
+
+            }
+            else if (LoadProfile.Checked == true)
+            {
+                process.StartInfo.FileName = "loadstate";
+                process.StartInfo.Arguments = StoreLocation.SelectedPath + @" /i:miguser.xml /i:migapp.xml";
+                process.Start();
+            } else
+            {
+                MessageBox.Show("Please don't click this button, yet.");
+            }
+        }
+    }
+}
